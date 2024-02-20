274e7ce49eb4be048e5a7ddc4d050e1cd1d790fc
prashansa012 <156915156+prashansa012@users.noreply.github.com>
2024-02-20 15:35:47 +0530

    use modular components
    two separate vue files 1. productgrid 2. producgriditem

---
+++ src/components/ProductGrid.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="grid-wrap">
+    <ProductsGrideItem v-for="product in products" :key="product.id" :product="product"/>
+
+  </div>
+</template>
+
+<script>
+import ProductGridItem from './ProductGridItem.vue';
+export default {
+   name:'ProductsGrid',
+   props:['products'],
+   components:{
+    ProductGridItem,
+   }
+}
+</script>
+
+<style scoped>
+ .grid-wrap {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin-top: 16px;
+  }
+</style>
