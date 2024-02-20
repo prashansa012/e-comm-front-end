b5eab478766f6d6023c3f80811c2bd871351b7e0
prashansa012 <156915156+prashansa012@users.noreply.github.com>
2024-02-20 14:55:29 +0530

    styled the CartPage, ProductDetailPage and the ProductPage
    and created all the headings and the buttons

--- src/views/CartPage.vue
+++ src/views/CartPage.vue
@@ -1,15 +1,76 @@
 <template>
-  <h3>this is Cart Page</h3>
+ <div id="page-wrap">
+   <h1>Shopping Cart</h1>
+   <div v-for="product in cartItems" :key="product.id" class="product-container">
+    <img class="product-image" :src="product.imageUrl"/>
+    <div class="details-wrap">
+      <h3>{{ product.name }}</h3>
+      <p>${{ product.price }}</p>
+    </div>  
+    <button class="remove-button">Remove from cart</button>
+  </div>
+  <h3 id="total-price">TOTAL: ${{ totalPrice }}</h3>
+  <button id="checkout-button">Proceed to Checkout</button>
+ </div>
 </template>
 
 <script>
+import { cartItems } from '@/fake-data';
 export default {
-  name:'CartPage'
+  name:'CartPage',
+  data(){
+    return{
+      cartItems,
+     
+    }
+  },
+  computed:{
+    totalPrice(){
+      return this.cartItems.reduce(
+        (sum,item)=>sum+Number(item.price),0
+      )
+    }
+  }
 }
 </script>
 
 <style>
-h3{
-    color: aquamarine;
+h1{
+ border-bottom: 1px solid black;
+ margin:0;
+ margin-top: 16px;
+ padding: 16px;
 }
+#total-price {
+    padding: 16px;
+    text-align: right;
+  }
+
+  #checkout-button {
+    width: 100%;
+  }
+
+  .product-container {
+    align-content: 'center';
+    border-bottom: 1px solid #ddd;
+    display: flex;
+    padding: 16px;
+    width: 100%;
+  }
+
+  .product-image {
+    flex: 1;
+    height: 100px;
+    max-width: 100px;
+  }
+
+  .details-wrap {
+    padding: 0 16px;
+    flex: 3;
+  }
+
+  .remove-button {
+    flex: 1;
+    margin: auto;
+  }
 </style>
